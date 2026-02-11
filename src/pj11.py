fee27fbb8681cf109b290fd58c1bd91fae6d2772
Ahmed Sheta <ahmedhesham73@gmail.com>
2026-02-11 22:41:01 +0100

    check dev push

--- src/pj11.py
+++ src/pj11.py
@@ -8,6 +8,10 @@ def main():
     # Print the current working directory
     print("Current Working Directory:", current_directory)
 
+    # List all files and directories in the current working directory
+    items = os.listdir(current_directory)
+    print("Files and Directories in Current Working Directory:")
+
 
 if __name__ == "__main__":
     main()
