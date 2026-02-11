8f9d0d557f91ff89c46309a19c5c8fe2de44b2c6
Ahmed Sheta <ahmedhesham73@gmail.com>
2026-02-11 21:30:20 +0100

    minor change

--- src/pj11.py
+++ src/pj11.py
@@ -1,10 +1,9 @@
 import os
 
+
 def main():
     # Get the current working directory
     current_directory = os.getcwd()
-    
+
     # Print the current working directory
     print("Current Working Directory:", current_directory)
-
-
