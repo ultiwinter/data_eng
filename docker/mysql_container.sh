5460efc700a3c5c43674881f124a85b7a33b0379
Ahmed Sheta <95130956+ultiwinter@users.noreply.github.com>
2026-03-01 17:32:27 +0100

    Change MySQL container port mapping to 3306

--- docker/mysql_container.sh
+++ docker/mysql_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d  --name mysql-hn  -p 7000:7000  -e MYSQL_ROOT_PASSWORD=root  -e MYSQL_DATABASE=hn  mysql:8.0
+docker run -d  --name mysql-hn  -p 7000:3306  -e MYSQL_ROOT_PASSWORD=root  -e MYSQL_DATABASE=hn  mysql:8.0
